c1ecb828f29fa8d3d1015fe3fa95abb07d8e64c2
Cannonbarrel <Firenoob1001@gmail.com>
2026-07-14 00:26:45 -0400

    adding transfer / savings account mechanism

--- script.py
+++ script.py
@@ -1,11 +1,8 @@
 from abc import ABC, abstractmethod
 
-
-
 class Runner:
 
     users = []
-
 
     @staticmethod
     def initialize_users():
@@ -14,31 +11,28 @@ class Runner:
         admin.set_username("admin")
         admin.set_password("admin123")
 
-
         customer1 = Customer()
         customer1.set_username("cannon")
         customer1.set_password("cannon123")
         customer1.add_account(CheckingAccount())
-
+        customer1.add_account(SavingsAccount())
 
         customer2 = Customer()
         customer2.set_username("bannon")
         customer2.set_password("bannon123")
+        customer2.add_account(CheckingAccount())
         customer2.add_account(SavingsAccount())
-
 
         customer3 = Customer()
         customer3.set_username("shannon")
         customer3.set_password("shannon123")
         customer3.add_account(CheckingAccount())
-
+        customer3.add_account(SavingsAccount())
 
         Runner.users.append(admin)
         Runner.users.append(customer1)
         Runner.users.append(customer2)
         Runner.users.append(customer3)
-
-
 
     @staticmethod
     def main():
@@ -46,7 +40,6 @@ class Runner:
         Runner.initialize_users()
 
         print("Welcome to ABC Digital Bank")
-
 
         running = True
 
@@ -59,23 +52,18 @@ class Runner:
 
                 print("Invalid Credentials")
 
-
             elif login_result == "admin":
 
                 Runner.admin_dashboard()
-
 
             else:
 
                 Runner.customer_dashboard(login_result)
 
-
-
             answer = input("\nContinue? y/n: ")
 
             if answer.lower() == "n":
                 running = False
-
 
     @staticmethod
     def login():
@@ -84,21 +72,15 @@ class Runner:
             "Enter username and password separated by space: "
         )
 
-
         parts = credentials.split()
-
 
         if len(parts) != 2:
             return "invalid"
 
-
         username = parts[0]
         password = parts[1]
 
-
-
         for user in Runner.users:
-
 
             if (user.get_username() == username and
                 user.get_password() == password):
@@ -110,16 +92,12 @@ class Runner:
 
         return "invalid"
 
-
-
     @staticmethod
     def customer_dashboard(username):
 
         customer = None
 
-        # Find the customer object
         for user in Runner.users:
-
             if user.get_username() == username:
                 customer = user
                 break
@@ -135,15 +113,17 @@ class Runner:
             print("\n1. View Account")
             print("2. Deposit")
             print("3. Withdraw")
-            print("4. Logout")
+            print("4. Transfer")
+            print("5. Logout")
 
             choice = input("Choose option: ")
 
             if choice == "1":
 
-                for account in customer.accounts:
+                for i, account in enumerate(customer.accounts):
                     print(
-                        "Account Type:",
+                        i + 1,
+                        "-",
                         type(account).__name__,
                         "Balance: $",
                         account.balance
@@ -151,50 +131,76 @@ class Runner:
 
             elif choice == "2":
 
+                account_num = int(input("Choose account number: "))
+
+                if account_num < 1 or account_num > len(customer.accounts):
+                    print("Invalid account.")
+                    continue
+
                 amount = float(input("Deposit amount: "))
 
-                account = customer.accounts[0]
-
-                account.deposit(amount)
+                customer.accounts[account_num - 1].deposit(amount)
 
             elif choice == "3":
 
+                account_num = int(input("Choose account number, 1 for checking 2 for savings: "))
+
+                if account_num < 1 or account_num > len(customer.accounts):
+                    print("Invalid account.")
+                    continue
+
                 amount = float(input("Withdraw amount: "))
 
-                account = customer.accounts[0]
-
-                account.withdraw(amount)
-
+                customer.accounts[account_num - 1].withdraw(amount)
 
             elif choice == "4":
 
+                print("\nYour Accounts")
+
+                for i, account in enumerate(customer.accounts):
+                    print(i + 1, "-", type(account).__name__)
+
+                from_account = int(input("Transfer from account: "))
+                to_account = int(input("Transfer to account: "))
+
+                if (from_account < 1 or
+                        from_account > len(customer.accounts) or
+                        to_account < 1 or
+                        to_account > len(customer.accounts)):
+                    print("Invalid account.")
+                    continue
+
+                if from_account == to_account:
+                    print("Cannot transfer to the same account.")
+                    continue
+
+                amount = float(input("Transfer amount: "))
+
+                customer.accounts[from_account - 1].transfer(
+                    amount,
+                    customer.accounts[to_account - 1]
+                )
+
+            elif choice == "5":
+
                 print("Logging out...")
                 break
-
-
 
             else:
 
                 print("Invalid option")
 
-
-
     @staticmethod
     def admin_dashboard():
 
-
         while True:
-
 
             print("\nADMIN MENU")
             print("1. View Customers")
             print("2. Add Customer")
             print("3. Logout")
 
-
             choice = input("Choose option: ")
-
-
 
             if choice == "1":
 
@@ -207,8 +213,6 @@ class Runner:
                         print(
                             user.get_username()
                         )
-
-
 
             elif choice == "2":
 
@@ -238,58 +242,41 @@ class Runner:
 
                 break
 
-
-
 class User(ABC):
-
 
     def __init__(self):
 
         self.username = ""
         self.password = ""
 
-
-
     def get_username(self):
 
         return self.username
-
-
 
     def set_username(self, username):
 
         self.username = username
 
-
-
     def get_password(self):
 
         return self.password
 
-
-
     def set_password(self, password):
 
         self.password = password
-
-
 
     @abstractmethod
     def get_user_type(self):
 
         pass
 
-
 class Admin(User):
-
 
     def get_user_type(self):
 
         return "admin"
 
-
 class Customer(User):
-
 
     def __init__(self):
 
@@ -297,49 +284,33 @@ class Customer(User):
 
         self.accounts = []
 
-
-
     def add_account(self, account):
 
         self.accounts.append(account)
-
-
 
     def get_user_type(self):
 
         return "customer"
 
-
 class AccountOperations(ABC):
-
 
     @abstractmethod
     def deposit(self, amount):
 
         pass
 
-
-
     @abstractmethod
     def withdraw(self, amount):
 
         pass
-
-
 
     @abstractmethod
     def transfer(self, amount, account):
 
         pass
 
-
-
 class Account(ABC):
-
-
     counter = 1000
-
-
 
     def __init__(self):
 
@@ -348,8 +319,6 @@ class Account(ABC):
         self.id = Account.counter
 
         self.balance = 0
-
-
 
 class CheckingAccount(Account, AccountOperations):
 
@@ -360,8 +329,6 @@ class CheckingAccount(Account, AccountOperations):
 
         print("Deposit successful")
 
-
-
     def withdraw(self, amount):
 
         if amount <= self.balance:
@@ -373,9 +340,6 @@ class CheckingAccount(Account, AccountOperations):
         else:
 
             print("Insufficient funds")
-
-
-
 
     def transfer(self, amount, account):
 
@@ -390,9 +354,6 @@ class CheckingAccount(Account, AccountOperations):
         else:
 
             print("Insufficient funds")
-
-
-
 
 class SavingsAccount(Account, AccountOperations):
 
@@ -403,9 +364,6 @@ class SavingsAccount(Account, AccountOperations):
 
         print("Deposit successful")
 
-
-
-
     def withdraw(self, amount):
 
         if amount <= self.balance:
@@ -417,9 +375,6 @@ class SavingsAccount(Account, AccountOperations):
         else:
 
             print("Insufficient funds")
-
-
-
 
     def transfer(self, amount, account):
 
@@ -434,6 +389,4 @@ class SavingsAccount(Account, AccountOperations):
         else:
 
             print("Insufficient funds")
-
-
 Runner.main()
