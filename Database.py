0418d417aa4cb33106ee2ce9cbbbd4d6550691a5
Zachary Cannon <Firenoob1001@gmail.com>
2026-07-17 13:30:43 -0400

    Update Database.py

--- Database.py
+++ Database.py
@@ -4,7 +4,7 @@ from flask_pymongo import PyMongo
 
 
 
-# 2. This safely encodes any special characters (like @ or !) into URL-safe formats
+#  This safely encodes any special characters (like @ or !) into URL-safe formats
 safe_username = urllib.parse.quote_plus(DB_USERNAME)
 safe_password = urllib.parse.quote_plus(DB_PASSWORD)
 
@@ -12,7 +12,7 @@ safe_password = urllib.parse.quote_plus(DB_PASSWORD)
 mongo = PyMongo()
 
 def init_db(app):
-    # 3. We inject the safely encoded strings directly into the URI string
+    # We inject the safely encoded strings directly into the URI string
     app.config[
         "MONGO_URI"] = f"mongodb+srv://{safe_username}:{safe_password}@cluster0.roeba7n.mongodb.net/my_rest_api_db?retryWrites=true&w=majority"
     mongo.init_app(app)
